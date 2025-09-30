9fd0b79ea85b624c4e55025b981b732d71198bee
Florian Bellmann <florianbellmann91@gmail.com>
2025-09-30 16:07:53 +0200

    plastic surgery

--- client/src/components/Chat/Messages/MessagesView.tsx
+++ client/src/components/Chat/Messages/MessagesView.tsx
@@ -45,7 +45,7 @@ function MessagesViewContent({
               width: '100%',
             }}
           >
-            <div className="flex flex-col pb-9 dark:bg-transparent">
+            <div className="flex flex-col pt-6 pb-9 dark:bg-transparent">
               {(_messagesTree && _messagesTree.length == 0) || _messagesTree === null ? (
                 <div
                   className={cn(
